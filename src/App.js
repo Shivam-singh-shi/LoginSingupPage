1000df2ce53a243a76f187caf391736ef12bdfb5
Shivam Singh <shivamsinghwq73@gmail.com>
2026-07-02 11:15:18 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function App() {
+  return (
+     <div><Authform/></div>
+     //app.js mai kuch bhi likhwnge wo browser mai show hoga.
+  );
+}
+
+
+export default App;
---
+++ src/Authform.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+
+export default function Authform() {
+  const [isLogin, setIsLogin] = useState(true); //kisi state ko krne hota hai to usestate hook use krte hai
+  return (
+    <div className="container">
+      <div className="form-container">
+        <div className="form-toggle">
+          <button
+            className={isLogin ? "active " : ""}
+            onClick={() => setIsLogin(true)}
+          >
+            Login
+          </button>
+          <button
+            className={!isLogin ? "active" : ""}
+            onClick={() => setIsLogin(false)}
+          >  
+            Singup
+          </button>
+        </div>
+        {isLogin ? (
+          <>
+            <div className="form">
+              <h2>Login Form</h2>
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <a href="#">Forgot Password</a>
+              <button>Login</button>
+              <p>
+                Not a Member?
+                <a href="#" onClick={() => setIsLogin(false)}>
+                  Singup Now
+                </a>
+              </p>
+            </div>
+          </>
+        ) : 
+          <>
+            <div className="form">
+              <h2>Singup Form</h2>
+              <input type="email" placeholder="Email" />
+              <input type="password" placeholder="Password" />
+              <input type="password" placeholder=" Confirm Password" />
+              <button>SingUp</button>
+            </div>
+          </>
+        }
+      </div>
+    </div>
+  );
+}
